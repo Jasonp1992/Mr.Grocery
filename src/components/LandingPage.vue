99a57bf2f4d360cbc37a3dbf5e22d7e6f6e4990b
jason <potvinj@gmail.com>
2023-02-27 18:50:17 -0500

    start adding content to landing page

--- src/components/LandingPage.vue
+++ src/components/LandingPage.vue
@@ -39,4 +39,32 @@
             </aside>
         </section>
     </header>
+    <div class="bg-pretty-blue h-2">
+    </div>
+    <article class="text-black ml-14 mr-14 mb-4 mt-4">
+        <h2 class="font-indie-flower text-2xl">recipes</h2>
+        <section class="min-w-400 max-w-400">
+            <p class="mt-4">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            </p>
+            <p class="mt-4">
+                Ligula ullamcorper malesuada proin libero nunc consequat interdum varius sit. Ullamcorper morbi tincidunt ornare massa eget egestas purus. 
+            </p>
+            <p class="mt-4">
+                Hac habitasse platea dictumst quisque.
+            </p>
+        </section>
+    </article>
+    <article class="text-black">
+        <h2 class="font-indie-flower text-2xl">Shopping List</h2>
+                <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            </p>
+            <p>
+                 Ligula ullamcorper malesuada proin libero nunc consequat interdum varius sit. Ullamcorper morbi tincidunt ornare massa eget egestas purus. 
+            </p>
+            <p>
+                Hac habitasse platea dictumst quisque.
+            </p>
+    </article>
 </template>
