5f19924373d6c4bc687dd70977cf1631ba67f401
jason <potvinj@gmail.com>
2023-02-27 18:15:05 -0500

    Create page and set up header

---
+++ src/components/LandingPage.vue
@@ -0,0 +1,42 @@
+<script setup>
+
+</script>
+
+<template>
+    <header class="bg-slick-gray h-701">
+        <nav class="flex m-6">
+            <h1 class="flex-auto font-indie-flower text-4xl">
+                Untitled Grocery App
+            </h1>
+            <!-- im going to need some feedback when clicked -->
+            <button class="bg-pretty-blue p-2 pl-4 pr-4 rounded-2xl drop-shadow-lg">
+                Log In
+            </button>
+        </nav>
+        <section class="flex m-14 justify-around items-center gap-20">
+            <section>
+                <article class="min-w-400 text-lg">
+                    <p>
+                        A new way to plan your shopping trips
+                    </p>
+                    <p class="mt-5">
+                        Untitled Grocery App is your one stop shop for finding recipes and creating organized shopping lists <br>
+                    </p>
+                    <p class="mt-5">
+                        Find a recipe you like just add it to your weekly list and Untitled Grocery App automatically creates an organized list for you. <br>
+                    </p>
+                    <p class="mt-5">
+                        So try it out today!! <br>
+                    </p>
+                </article>
+
+                <button class="bg-pretty-blue p-3 pl-6 pr-6 rounded-2xl drop-shadow-lg mt-6">
+                    Free Sign Up
+                </button>
+            </section>
+            <aside>
+                <img  class="h-500 max-w-560 rounded-3xl border-2 border-slate-500 origin-center -rotate-6" src="/img/grocery-basket.jpg" />
+            </aside>
+        </section>
+    </header>
+</template>
