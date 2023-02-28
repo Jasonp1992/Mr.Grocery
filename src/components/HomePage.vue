18f48db7f4402f847db90205d9555a45159e0340
jason <potvinj@gmail.com>
2023-02-28 17:39:50 -0500

    create home page

---
+++ src/components/HomePage.vue
@@ -0,0 +1,11 @@
+<script setup>
+    import Header from './Header.vue';
+    import Footer from './Footer.vue';
+    import HomeSearchImage from './HomeSearchImage.vue';
+</script>
+
+<template>
+    <Header />
+    <HomeSearchImage />
+    <Footer />
+</template>
