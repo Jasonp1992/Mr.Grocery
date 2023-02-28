e81f160f406bbdcc79f34dc2b515fb4a6cc26b6a
jason <potvinj@gmail.com>
2023-02-28 17:39:20 -0500

    set up style for search area

---
+++ src/components/HomeSearchImage.vue
@@ -0,0 +1,10 @@
+<script setup>
+</script>
+
+<template>
+    <section class="bg-search-image h-96 bg-center bg-cover flex flex-col justify-center items-center gap-4">
+        <h2 class="text-3xl text-off-blue font-josefin-sans">Search Recipes</h2>
+        <input type="text" class="w-80 p-2 text-black drop-shadow-lg">
+    </section>
+    <div class="bg-pretty-blue h-2"></div>
+</template>
