95e5ff898117da756fd9b14f23f828c247bb4d22
jason <potvinj@gmail.com>
2023-02-28 14:57:58 -0500

    set up flow between landing page and home page

--- src/App.vue
+++ src/App.vue
@@ -2,15 +2,26 @@
   import todo from "./components/todo.vue";
   import Header from "./components/Header.vue";
   import Footer from "./components/Footer.vue";
+  import LandingPage from "./components/LandingPage.vue";
+  import { ref } from 'vue'
+  import { db, auth } from './firebase/index'
+
+  let isLoggedIn = ref(true)
+
+  auth.onAuthStateChanged(function (user) {
+    if (user) {
+      isLoggedIn.value = true
+      console.log("logged in: ", auth)
+    } else {
+      isLoggedIn.value = false
+      console.log("logged out: ", auth)
+    }
+  })
 </script>
 
 <template>
-  <Header />
-  <div class="flex justify-between">
-    <RouterView></RouterView>
-    <todo class="min-w-max"/>
-  </div>
-  <Footer />
+  <Header v-if="isLoggedIn" />
+  <LandingPage v-else />
 </template>
 
 <style scoped>
