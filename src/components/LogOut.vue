21ccb2b3e2bb5ded8c8539496aac7a8d67917482
jason <potvinj@gmail.com>
2023-02-28 16:45:13 -0500

    create component for log out button

---
+++ src/components/LogOut.vue
@@ -0,0 +1,17 @@
+<script setup>
+    import { auth } from '../firebase/index'
+
+    const logOut = () => {
+        auth.signOut()
+            .then(function () {
+                console.log("Signed Out", auth)
+            })
+            .catch((error) => {
+                console.log("Sign Out Error", error)
+            })
+    }
+</script>
+
+<template>
+        <button class="bg-pretty-blue p-1 pl-4 pr-4 rounded-2xl drop-shadow-lg max-h-10" @click="logOut">Log Out</button>
+</template>
